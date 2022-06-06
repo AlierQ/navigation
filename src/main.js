8b62da7e670316d5ac56bea9f549114d91be63a5
AlierQ <1029369938@qq.com>
2022-06-06 11:20:38 +0800

    删除站点

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-// let siteList = $('.site');
-// let url = $(siteList[0]).find('a').attr('href')
-// console.log(url);
-// console.log($("#div").load( "http://www.bilibili.com" + " a[id^='page']"))
-
 $(function(){
     let urls= $('.site > a');
     urls.each((index,item)=>{
@@ -14,10 +9,37 @@ $(function(){
                 if(title.textContent === '')
                     title.textContent = data;
             },
-            error:function(){}
+            error:function(){
+                let title = $(item).find('.site-title')[0]
+                title.textContent = '╮(╯▽╰)╭  长按命名';
+            }
         })
-        
         // 动态加载favicon
         $(item).find('.logo img')[0].src = 'http://47.95.207.116:18002/api/v1/favicon?url=' + item.href;
+    })
+})
+
+// a标签事件
+$('.site > a').each((index,item)=>{
+    item.addEventListener('click',function(e){
+        e.preventDefault();
+        window.location.href = item.href
+    })
+
+    item.addEventListener('mousedown',function(){
+        setTimeout(()=>{
+            $(item).find('.site-title')[0].textContent = prompt('输入名称：');
+        },300)
+    })
+})
+
+// 删除按钮事件
+$('.delete').each((index,item)=>{
+    item.addEventListener('click',function(e){
+        console.log(e);
+        $(item).parent().parent().parent().remove()
+        e.preventDefault();
+        // 阻止冒泡
+        e.stopPropagation();
     })
 })
